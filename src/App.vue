c77824231043e6d24fd4880274ff4cb26767f0b8
Kapatych <onetruestory@ya.ru>
2020-09-03 12:13:57 +0300

    Added simple header and footer, also create client start layot

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,66 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <TheHeader />
+    <main class="main">
+      <CreateClient />
+    </main>
+    <TheFooter />
+  </div>
 </template>
 
 <script>
+import TheHeader from "@/components/TheHeader";
+import TheFooter from "@/components/TheFooter";
+import CreateClient from "@/views/CreateClient";
 export default {
   name: "App",
-  components: {}
+  components: { TheHeader, TheFooter, CreateClient }
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+*,
+:after,
+:before {
+  box-sizing: inherit;
+}
+html {
+  box-sizing: border-box;
+  height: 100%;
+  font-family: "Open Sans";
+}
+body {
+  min-width: 320px;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  font-size: 14px;
+  line-height: 24px;
+  font-style: normal;
+}
+img {
+  max-width: 100%;
+  height: auto;
+}
+p {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.container {
+  margin: 0 auto;
+  padding: 0 20px;
+  max-width: 1200px;
+}
+.main {
+  flex: 1 0 auto;
+  background: url(assets/background.jpg) 50% no-repeat;
+  background-size: cover;
+  border-top: 1px solid rgba(31, 32, 65, 0.1);
+  border-bottom: 1px solid rgba(31, 32, 65, 0.1);
+}
+</style>
---
+++ src/views/CreateClient.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="create-client">
+    <div class="create-client__container">
+      <div class="create-client__wrapper">
+        <h1 class="create-client__title">Новый клиент</h1>
+        <form @click.prevent="submitHandler">
+          <input type="text" name="name" autocomplete="off" />
+          <h3>Адрес</h3>
+          <h3>Паспорт</h3>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss">
+.create-client {
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+.create-client__container {
+  width: 100%;
+  max-width: 420px;
+  margin: 0 auto;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+}
+.create-client__wrapper {
+  padding: 40px 30px 30px;
+  background: #fff;
+  border: 1px solid rgba(31, 32, 65, 0.1);
+  box-shadow: 0 0 25px rgba(31, 32, 65, 0.25);
+  border-radius: 4px;
+}
+.create-client__title {
+  margin-top: 0;
+}
+</style>
